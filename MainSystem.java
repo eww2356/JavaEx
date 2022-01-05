d9a86c4cb879785cb1442a95a0c5997b17f0e74f
eww2356 <45067949+eww2356@users.noreply.github.com>
2022-01-05 12:02:31 +0900

    Update MainSystem.java

--- MainSystem.java
+++ MainSystem.java
@@ -6,7 +6,7 @@ public class MainSystem {
 	static Log logger = new Log();
 	
 	public static void main(String[] args) {
-    // 에러 발생 시 로그 기록 테스트를 위한 코드
+    		// 에러 발생 시 로그 기록 테스트를 위한 코드
 		try {
 			throw new Exception("에러가 발생했습니다!!!");
 		} catch (Exception e) {
